c7cebf245b824dccc36da065c314b3b6e09c5615
aryan8292 <94910943+aryan8292@users.noreply.github.com>
2024-10-17 17:27:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,48 +1,62 @@
+#(©)CodeXBotz
+
+
+
+
 import os
 import logging
 from logging.handlers import RotatingFileHandler
 
+
+
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6751025808:AAEiA1jqQ5YI0GEeLOiKscMcXNzueYxsi8E")
-APP_ID = int(os.environ.get("APP_ID", "24466877"))
-API_HASH = os.environ.get("API_HASH", "403596c01ecce435211d06eda69e9ad6")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7279617995:AAFt6zDyJTbf1sa1K2UU6jOfYD7V7l3uDK4")
+
+#Your API ID from my.telegram.org
+APP_ID = int(os.environ.get("APP_ID", "23004940"))
+
+#Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "25c013f901474cffcad6a9d03e3a06bb")
+
+#Your db channel Id
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001517345433"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "5079629749"))
-PORT = os.environ.get("PORT", "8050")
+
+#Port
+PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = "mongodb+srv://VAISHNAV:VAISHNAV@cluster0.sn8ij4b.mongodb.net/?retryWrites=true&w=majority"
-DB_NAME = os.environ.get("DATABASE_NAME", "VAISHNAV")
+DB_URI = "mongodb+srv://sunitverma080:lwYnQJB7WLC50xho@lodubot1.5w14taf.mongodb.net/?retryWrites=true&w=majority&appName=lodubot1"
+DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
-#Shortner (token system) 
-
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "runurl.in")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "0f018a7ba3958b3482773c597e823ffe3efc5a40")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "kingurl.in")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "ec0d51c38dae62a342b23339d58e5c90e8db5ff3")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 36000)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID", "https://t.me/How_to_Download_7x/35") 
+TUT_VID = os.environ.get("TUT_VID","gojfsi/2")
+
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "100"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}, Thanks for using me :D @team_netflix ⚡️.")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "5079629749").split()):
+    for x in (os.environ.get("ADMINS", "7208538115 6616938712").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ʜᴇʟʟᴏ {first}\n\n<b>ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ʀᴇʟᴏᴀᴅ button ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ʀᴇǫᴜᴇꜱᴛᴇᴅ ꜰɪʟᴇ.</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>• ʙʏ </b>")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "This video/Photo/anything is available on the internet. We LeakHubd or its subsidiary channel doesn't produce any of them.")
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
@@ -51,11 +65,10 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "ʙᴀᴋᴋᴀ ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ꜱᴇɴᴘᴀɪ!!"
+USER_REPLY_TEXT = "Join Posting Channel From  @Direct_vid For More Links/Files!"
 
-ADMINS.append(OWNER_ID)
-ADMINS.append(5079629749)
-ADMINS.append(5079629749)
+ADMINS.append(6367302898)
+ADMINS.append(7208538115)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
